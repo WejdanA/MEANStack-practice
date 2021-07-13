050c463788230427abf8f6b76cf892feba6d4dfd
WejdanA <eng.wejdan.a@gmail.com>
2021-07-13 13:13:16 +0300

    JS practice

--- JS/learnJS.js
+++ JS/learnJS.js
@@ -5,3 +5,13 @@ obj.name="John";
 delete obj.name;
 console.log(obj.name) ;
 console.log(obj.lastname) ;
+
+function Obj2(name, lastname) {
+    this.name = name;
+    this.lastname = lastname;
+   
+  };
+  
+  const me = new Obj2("wejdan", "Alghamdi");
+  console.log(me.name) ;
+console.log(me.lastname) ;
